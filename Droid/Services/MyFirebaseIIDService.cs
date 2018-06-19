745ff17962e690f724d77a7b2615dc8f90167ec1
ac <andrew.chung@xamarin.com>
2018-06-19 13:22:03 -0700

    triggering and receiving template notifications
    triggering and receiving template notifications

--- Droid/Services/MyFirebaseIIDService.cs
+++ Droid/Services/MyFirebaseIIDService.cs
@@ -21,20 +21,55 @@ namespace pushsample.Droid.Services
             var refreshedToken = FirebaseInstanceId.Instance.Token;
             Log.Debug(TAG, "FCM token: " + refreshedToken);
             Console.WriteLine(TAG, "FCM token: " + refreshedToken);
-            SendRegistrationToServer(refreshedToken);
+            //SendNativeRegistrationToServer(refreshedToken);
+            //SendTemplateRegistrationToServer(refreshedToken);
+            SendTemplateMultipleRegistrationToServer(refreshedToken);
         }
 
-        void SendRegistrationToServer(string token)
+        void SendNativeRegistrationToServer(string token)
         {
         // Register with Notification Hubs
         hub = new NotificationHub(App.NotificationHubName,
                                       App.ConnectionString, this);
 
-        var tags = new List<string>() { };
+        //var tags = new List<string>() { };
+        var tags = new List<string>() { "World", "Politics", "Business", "Technology", "Science", "Sports"};
         var regID = hub.Register(token, tags.ToArray()).RegistrationId;
 
         Log.Debug(TAG, $"Successful registration of ID {regID}");
         Console.WriteLine(TAG, $"Successful registration of ID {regID}");
+        }
+
+        void SendTemplateRegistrationToServer(string token)
+        {
+            // Register with Notification Hubs
+            hub = new NotificationHub(App.NotificationHubName, App.ConnectionString, this);
+
+            //var tags = new List<string>() { };
+            var tags = new List<string>() { "World" };
+
+            String templateBodyGCM = "{\"data\":{\"message\":\"$(messageParam)\"}}";
+
+            var regID = hub.RegisterTemplate(token, "SimpleGCMTemplate", templateBodyGCM, tags.ToArray()).RegistrationId;
+
+            Log.Debug(TAG, $"Successful registration of ID {regID}");
+            Console.WriteLine(TAG, $"Successful registration of ID {regID}");
+        }
+
+        void SendTemplateMultipleRegistrationToServer(string token)
+        {
+            // Register with Notification Hubs
+            hub = new NotificationHub(App.NotificationHubName, App.ConnectionString, this);
+
+            //var tags = new List<string>() { };
+            var tags = new List<string>() { "World", "Politics", "Business", "Technology", "Science", "Sports" };
+
+            String templateBodyGCM = "{\"data\":{\"message\":\"$(messageParam)\"}}";
+
+            var regID = hub.RegisterTemplate(token, "SimpleGCMTemplate", templateBodyGCM, tags.ToArray()).RegistrationId;
+
+            Log.Debug(TAG, $"Successful registration of ID {regID}");
+            Console.WriteLine(TAG, $"Successful registration of ID {regID}");
         }
     }
 }
